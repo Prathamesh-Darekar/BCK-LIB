0d7f9733690a643ad831c27bf5e4a1be519e7f86
Prathamesh-Darekar <darekarprathamesh2004@gmail.com>
2023-09-25 08:35:22 -0700

    PDF linked

--- react-code/src/constants.jsx
+++ react-code/src/constants.jsx
@@ -24,19 +24,59 @@ export const allCoursesWD = [
             title: "1st Sem",
             subs: [
               {
-                title: "Java",
+                title: "Programming in C",
                 links: [
                   {
-                    link: "https://drive.google.com/file/d/1fXwpEdQMr-KlTcFJQagn-Tvia_LvaXu4/preview",
-                    title: "BCA_QP_JAVA_SEP_2S_2022.pdf"
+                    title:"I-SEM_BCA_2022_PROGRAMMING-IN-C",
+                    link:""
                   },
+                  {
+                    link: "https://drive.google.com/file/d/1gBERJap3qZpZeqNB4bxGOuScrOs_7col/preview",
+                    title: "I-SEM_BCA_2023_PROGRAMMING-IN-C"
+                  }
                 ],
               },
-              { title: "Maths", links: [] },
-              { title: "FIT", links: [] },
-              { title: "English", links: [] },
+              { title: "Maths", links: [
+                {
+                  title:"I-SEM_BCA_2022_MATHEMATICAL-FOUNDATION",
+                  link:""
+                },
+                {
+                  title:"I-SEM_BCA_2023_MATHEMATICAL-FOUNDATION",
+                  link:"https://drive.google.com/file/d/1gKDNxN1xbmczFSihiMi43zAN27Kp_cPt/preview"
+                }
+              ] },
+              { title: "FIT", links: [
+                {
+                  title:"I-SEM_BCA_2022_FUNDAMENTALS-OF-COMPUTER",
+                  link:""
+                },
+                {
+                  title:"I-SEM_BCA_2023_FUNDAMENTALS-OF-COMPUTER",
+                  link:"https://drive.google.com/file/d/1g8XkAS4V8VOLzRqUbQXwq2sX78TyPvo_/preview"
+                }
+              ] },
+              { title: "English", links: [
+                {
+                  title:"I-SEM_BCA_2022_GENERIC-ENGLISH",
+                  link:""
+                },
+                {
+                  title:"I-SEM_BCA_2023_GENERIC-ENGLISH",
+                  link:"https://drive.google.com/file/d/1gKX_Qhn3g52n_oUfttFa5FkUqkSpA97O/preview"
+                }
+              ] },
               { title: "Hindi/Sanskrit", links: [] },
-              { title: "OpenElective", links: [] },
+              { title: "kannada", links: [
+                {
+                  title:"I-SEM_BCA_2022_KANNADA",
+                  link:""
+                },
+                {
+                  title:"I-SEM_BCA_2023_KANNADA",
+                  link:"https://drive.google.com/file/d/1gLiv2nYfiZXGcv5fUxiHLX4R6QWD3sID/preview"
+                }
+              ] },
             ],
           },
           {
@@ -48,14 +88,50 @@ export const allCoursesWD = [
                   link: "https://drive.google.com/file/d/1fXwpEdQMr-KlTcFJQagn-Tvia_LvaXu4/preview"
                 },
                 {
-                  title: "II-SEM_BCA_2022_DISCRETE-MATHEMATICAL-STRUCTURES",
-                  link: "https://drive.google.com/file/d/1fXwpEdQMr-KlTcFJQagn-Tvia_LvaXu4/preview"
+                  title: "II-SEM_BCA_2023_DISCRETE-MATHEMATICAL-STRUCTURES",
+                  link: "https://drive.google.com/file/d/1gVWnCFEHSQS42X05ShQEamQcMmXD1wDM/preview"
                 }
               ] },
-              { title: "sub2", links: [] },
-              { title: "sub3", links: [] },
-              { title: "sub4", links: [] },
-              { title: "sub5", links: [] },
+              { title: "Data Structures", links: [
+                  {
+                    title:"II-SEM_BCA_2022_DATASTRUCTURE-USING-C",
+                    link:"https://drive.google.com/file/d/1gCJzvNUarh772hmHnwE0ATMiHhiOThm5/preview"
+                  },
+                  {
+                    title:"II-SEM_BCA_2023_DATASTRUCTURE-USING-C",
+                    link:"https://drive.google.com/file/d/1gLlzM0gxcgqXdNAAf6XfdBht4g4Gk4oR/preview"
+                  }
+              ] },
+              { title: "Java", links: [
+                {
+                  title:"II-SEM_BCA_2022_OBJECT-ORIENTED-CONCEPTS-USING-JAVA",
+                  link:"https://drive.google.com/file/d/1fPTl8b95a7JY7C7YcCJGaPnrvyg2hMOF/preview"
+                },
+                {
+                  title:"II-SEM_BCA_2023_OBJECT-ORIENTED-CONCEPTS-USING-JAVA",
+                  link:"https://drive.google.com/file/d/1gY_hs-Th1V3x94jhOt19oOV6-CCe-8Su/preview"
+                }
+              ] },
+              { title: "English", links: [
+                {
+                  title:"II-SEM_BCA_2022_GENERIC-ENGLISH",
+                  link:"https://drive.google.com/file/d/1fWR1Q53EpIYyg-fA1l0Z3lFt8YbyhwI2/preview"
+                },
+                {
+                  title:"II-SEM_BCA_2023_GENERIC-ENGLISH",
+                  link:"https://drive.google.com/file/d/1gV6UCzwfzLJvTmnRi8FjM1MM4CCyzPKN/preview"
+                }
+              ] },
+              { title: "kannada", links: [
+                {
+                  title:"II-SEM_BCA_2022_KANNADA",
+                  link:"https://drive.google.com/file/d/1fSfisXbzk8O2EFgPXp-KMjRQ81c7cKk8/preview"
+                },
+                {
+                  title:"II-SEM_BCA_2023_KANNADA",
+                  link:"https://drive.google.com/file/d/1jlhmuMsE84mne1JcJWUYotr5HI8qKcb2/preview"
+                }
+              ] },
               { title: "sub6", links: [] },
             ],
           },
@@ -68,11 +144,56 @@ export const allCoursesWD = [
           {
             title: "3rd Sem",
             subs: [
-              { title: "sub1", links: [] },
-              { title: "sub2", links: [] },
-              { title: "sub3", links: [] },
-              { title: "sub4", links: [] },
-              { title: "sub5", links: [] },
+              { title: "C#", links: [
+                {
+                  title:"III-SEM_BCA_2022_C#-AND-DOT-NET-FRAMEWORK",
+                  link:""
+                },
+                {
+                  title:"III-SEM_BCA_2023_C#-AND-DOT-NET-FRAMEWORK",
+                  link:"https://drive.google.com/file/d/1kOoH4rH8Hvh5TdIQVknIpUcO9eC83pmp/preview"
+                }
+              ] },
+              { title: "Networking", links: [
+                {
+                  title:"III-SEM_BCA_2022_COMPUTER-COMMUNICATION-AND-NETWORK",
+                  link:""
+                },
+                {
+                  title:"III-SEM_BCA_2023_COMPUTER-COMMUNICATION-AND-NETWORK",
+                  link:"https://drive.google.com/file/d/1kUiA56UFQKVNidPYsMnvQXL9ZxlY3ji6/preview"
+                }
+              ] },
+              { title: "DBMS", links: [
+                {
+                  title:"III-SEM_BCA_2022_DATABASE-MANAGEMENT-SYSTEM",
+                  link:""
+                },
+                {
+                  title:"III-SEM_BCA_2023_DATABASE-MANAGEMENT-SYSTEM",
+                  link:"https://drive.google.com/file/d/1kUWgYz_YNbPe-k1abLjdeUeQ7kPQ5-tS/preview"
+                }
+              ] },
+              { title: "English", links: [
+                {
+                  title:"III-SEM_BCA_2022_ENGLISH",
+                  link:""
+                },
+                {
+                  title:"III-SEM_BCA_2023_ENGLISH",
+                  link:"https://drive.google.com/file/d/1kbgoIUrjm0ST3kCedHGx4zaPB2lXOS7h/preview"
+                }
+              ] },
+              { title: "Hindi", links: [
+                {
+                  title:"III-SEM_BCA_2022_HINDI",
+                  link:""
+                },
+                {
+                  title:"III-SEM_BCA_2023_HINDI",
+                  link:"https://drive.google.com/file/d/1jpMagv92xzGUOXyzr0yvuqYNCLkEyGbv/preview"
+                }
+              ] },
               { title: "sub6", links: [] },
             ],
           },
